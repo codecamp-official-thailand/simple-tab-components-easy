dee8270ee0a653723d1819294d19ad68c9fd28fc
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-06-25 11:27:14 +0700

    init proj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    tabContents: [
+      { name: "tab1", content: "This is article One" },
+      { name: "tab2", content: "This is article Two" },
+      { name: "tab3", content: "This is article Three" },
+      { name: "tab4", content: "This is article Four" },
+    ],
+    currentTab: "tab1"
+  };
+
+  changeCurrentTab = (nextTabName) => {
+    this.setState({ currentTab: nextTabName });
+  };
+
+  renderActicle = () => {
+    const targetTab = this.state.tabContents.find(tab => tab.name === this.state.currentTab);
+
+    return <p>{targetTab.content}</p>;
+  };
+
+  render() {
+    const { currentTab, tabContents } = this.state;
+
+    return (
+      <div className="App">
+        <div>
+          {tabContents.map(tab => {
+            let className = "Tab";
+            if (tab.name === currentTab) {
+              className = className + " Active";
+            }
+            return (
+              <div
+                className={className}
+                onClick={() => this.changeCurrentTab(tab.name)}
+              >
+                {tab.name}
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <p>
+            {this.renderActicle()}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
